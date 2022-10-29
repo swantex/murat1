5afdc8d55ec3d8aec60844077015f88e661efa7f
Nigamanth Srivatsan <112625750+nigamanthsrivatsan@users.noreply.github.com>
2022-10-29 20:28:22 +0530

    display something

--- backend/server/auth.php
+++ backend/server/auth.php
@@ -14,3 +14,17 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sent Message!</title>
+</head>
+<body>
+    <p>Sent the message</p>
+    <p>Message Sent: <?php echo $message; ?></p>
+</body>
+</html>
